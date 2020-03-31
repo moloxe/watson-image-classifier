b3e4d0ae6f92370c8d7a1f14374dcbaed6a37d0b
francoMG <francomolocheg99@gmail.com>
2020-03-31 13:30:45 -0500

    class side and score

--- public/main.js
+++ public/main.js
@@ -1,34 +1,34 @@
 const {
     ipcRenderer
 } = require('electron');
-const JSONFormatter = require('json-formatter-js')
 
 async function getVRresultsByUrl(ev) {
 
     const url = document.getElementById('input-url').value
     document.getElementById('loading').style.display = 'block'
+    document.getElementById('result').style.display = 'none'
 
     await ipcRenderer.invoke('get-visual-recognition-results-by-url', url).then(async (result) => {
-        const formatter = new JSONFormatter(result);
 
+
+        const ul = document.getElementById('predictions')
         let best = [0, ""]
         await result.forEach(ele => {
-            if(ele.score > best[0])
+
+            if (ele.score > best[0])
                 best = [best.score, ele.class]
+
+            const li = document.createElement('li')
+            li.className = "list-group-item"
+            const txt = `${ele.class}: ${ele.score}`
+            li.appendChild(document.createTextNode(txt))
+            ul.appendChild(li)
         });
-        const className = document.createElement('h1')
-        className.className = "text-center m-3"
-        className.innerText = best[1].toUpperCase()
 
-        const img = document.createElement('img')
-        img.src = url
-        img.className = "rounded mx-auto d-block w-50"
-
-        document.getElementById('result').innerHTML = "";
-        document.getElementById('result').appendChild(className)
-        document.getElementById('result').appendChild(img)
-        document.getElementById('result').appendChild(formatter.render())
+        document.getElementById('img').src = url
+        document.getElementById('title').innerText = (best[1].toUpperCase())
     })
 
     document.getElementById('loading').style.display = 'none'
+    document.getElementById('result').style.display = 'block'
 }
